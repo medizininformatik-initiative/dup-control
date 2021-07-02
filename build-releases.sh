c9f50a14a869c97bddf1224d0f2445da2257eab1
Jonas Wagner <jwagner@knoppiks.de>
2021-07-02 14:29:01 +0200

    Make Build Script Executable

--- build-releases.sh
+++ build-releases.sh
@@ -1,23 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 mkdir -p builds
 
-GOOS=linux   GOARCH=amd64  go build
+export CGO_ENABLED=0
+
+GOOS=linux   GOARCH=amd64 go build
 tar czf builds/polarctl-${version}-linux-amd64.tar.gz polarctl
 rm polarctl
 
-GOOS=linux  GOARCH=arm64  go build
+GOOS=linux   GOARCH=arm64 go build
 tar czf builds/polarctl-${version}-linux-arm64.tar.gz polarctl
 rm polarctl
 
-GOOS=darwin  GOARCH=amd64  go build
+GOOS=darwin  GOARCH=amd64 go build
 tar czf builds/polarctl-${version}-darwin-amd64.tar.gz polarctl
 rm polarctl
 
-GOOS=darwin  GOARCH=arm64  go build
+GOOS=darwin  GOARCH=arm64 go build
 tar czf builds/polarctl-${version}-darwin-arm64.tar.gz polarctl
 rm polarctl
 
-GOOS=windows GOARCH=amd64  go build
+GOOS=windows GOARCH=amd64 go build
 zip -q builds/polarctl-${version}-windows-amd64.zip polarctl.exe
 rm polarctl.exe
