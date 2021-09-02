907b43fa11946e746494a819be6e0d8ff5b7f5be
Jonas Wagner <jwagner@knoppiks.de>
2021-09-02 09:15:24 +0200

    Fix Windows Binary Name

--- build-releases.sh
+++ build-releases.sh
@@ -6,11 +6,11 @@ export CGO_ENABLED=0
 build() {
   GOOS=${1} GOARCH=${2} go build \
     -ldflags "-X git.smith.care/smith/uc-phep/polar/polarctl/cmd.Version=${version}" \
-    -o "builds/polarctl-${1}-${2}"
+    -o "builds/polarctl-${1}-${2}${3}"
 }
 
 build linux amd64
 build linux arm64
 build darwin amd64
 build darwin arm64
-build windows amd64
+build windows amd64 .exe
