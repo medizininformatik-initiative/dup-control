1699dd9556edcd405102c03e0145d1226b554f37
Jonas Wagner <jwagner@knoppiks.de>
2021-09-03 11:23:26 +0200

    Fix Binary Deployment Path

---
+++ deploy-releases.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+deploy() {
+  aws s3 cp "builds/polarctl-${1}-${2}" s3://polarctl/ --content-disposition "attachment; filename=\"polarctl${3}\""
+}
+
+deploy linux amd64
+deploy linux arm64
+deploy darwin amd64
+deploy darwin arm64
+deploy windows amd64 .exe
